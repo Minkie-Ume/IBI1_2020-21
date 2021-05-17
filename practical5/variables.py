425ceaf8c70de311b1af9d8535de2b3c09e8ca78
王舟玥 <minkie@wangzhouyuedeMacBook-Pro.local>
2021-05-18 00:06:07 +0800

    initialise

--- practical5/variables.py
+++ practical5/variables.py
@@ -11,3 +11,23 @@ print(d)
 print(e)
 print(z)
 print(w)
+
+#three way to compare d & e:
+#the first way:compare directly
+if d > e:
+	print('d is larger than e')
+else:
+	print('d is smaller than e')
+
+#the second way:
+f=d-e
+if f>0:
+	print('d is larger than e')
+else:
+	print('d is smaller than e')
+#the third way:
+f=d/e
+if f>1:
+	print('d is larger than e')
+else:
+	print('d is equal to e')
