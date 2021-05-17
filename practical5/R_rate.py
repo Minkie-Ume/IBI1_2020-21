183856945e9b3ae7372a06f35f2215c1559a0af5
王舟玥 <minkie@wangzhouyuedeMacBook-Pro.local>
2021-05-18 00:58:36 +0800

    initialise

--- practical5/R_rate.py
+++ practical5/R_rate.py
@@ -1,8 +1,12 @@
 n = 84
+#set the initial number of patients
 r = 1.2
+#assuming that the r rate equals to 1.2
 i = 1
+#calculate n in 5 generations.
 while i in range(1,6):
-	n = n + n * r
-	i = i + 1
+        n = n + n * r#calculate the new total number of n after a new generation
+        i = i + 1
 print(n)
- 
+
+
