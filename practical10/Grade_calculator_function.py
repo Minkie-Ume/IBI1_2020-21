c9d11f3ddb660ef26cdf15c0cc8a9b4905124ae6
王舟玥 <minkie@wangzhouyuedeMacBook-Pro.local>
2021-05-18 20:52:22 +0800

    initialise

--- practical10/Grade_calculator_function.py
+++ practical10/Grade_calculator_function.py
@@ -1,11 +1,11 @@
-class grade(object):
-	def __init__(self,s,x,y,z):
-		self.s=s
-		self.x=x
-		self.y=y
-		self.z=z
-		score= 0.4*x+0.3*y+0.3*z
-		print(self.s+'    '+str(score))
+def grade(name,portfolio,poster,mcq):#collect the information including name and the grade of portfolio,poster,mcq.
+	score= 0.4*portfolio+0.3*poster+0.3*mcq
+	return name,score
 
+#as an example, and print out the result with name and grade
 a=grade('Wang Zhouyue',70,80,90)
+name = a[0]
+grade=a[1]
+print('name:'+str(name)+'     '+'grade:'+str(grade))
+
 
