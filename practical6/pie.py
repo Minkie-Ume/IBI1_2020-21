4e0d3d0a0ff8a928108aab4474dcfc76f24f916a
王舟玥 <minkie@wangzhouyuedeMacBook-Pro.local>
2021-05-18 17:00:29 +0800

    initialise

--- practical6/pie.py
+++ practical6/pie.py
@@ -1,8 +1,15 @@
 import matplotlib.pyplot as plt
 #Pie chart, where the slices will be ordered and plotted counter-clockwise:
-sizes = [29862124, 11285561, 11205972, 4360823, 4234924]
-countries = ['USA', 'India', 'Brazil', 'Russia', 'UK']
-#specifies the fraction of the radius with which to offset each wedge
+#set a new frequency dictionary
+Countries = {'USA':29862124, 'India':11285561, 'Brazil':11205972, 'Russia':4360823, 'UK':4234924}
+#compute the total number
+total=sum(list(Countries.values()))
+#compute the frequency
+frequency={k:m/total*100 for k, m in Countries.items()}
+#make the pie chart
+sizes = list(frequency.values())
+countries = list(frequency.keys())
+#give sides and labels
 plt.pie(sizes, 
        labels=countries,  
        autopct='%1.1f%%', 
@@ -10,6 +17,7 @@ plt.pie(sizes,
         explode = [0,0,0,0,0],  
         shadow=False  
        )
+#give the pie chart's information
 plt.axis('equal')
 #Equal aspect ratio ensures that pie is drawn as a circle.
 plt.title("the proportion of infections")
