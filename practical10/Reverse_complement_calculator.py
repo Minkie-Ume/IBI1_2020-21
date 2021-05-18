1f705a38e70cdeee6ed07047f3eccd0ebaa30cc5
王舟玥 <minkie@wangzhouyuedeMacBook-Pro.local>
2021-05-18 20:11:43 +0800

    initialise

--- practical10/Reverse_complement_calculator.py
+++ practical10/Reverse_complement_calculator.py
@@ -4,9 +4,9 @@ def reverse_gene(G1):#define a function to get reverse DNA sequence
 	reverse_sequence=''
 	for i in G1:
 		a = i
-		b = table[i]
+		b = table[i]#get the reverse base
 		reverse_sequence += b
-	return reverse_sequence[::-1]
+	return reverse_sequence[::-1]#get the sequence reversely.
 
 reverse_gene("ACTTTACGGAT")#G1 is the specified DNA sequence
 
