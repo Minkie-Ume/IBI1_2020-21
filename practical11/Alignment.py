d6ede80030efff63b12b3c3705a471e1521bbda9
王舟玥 <minkie@wangzhouyuedeMacBook-Pro.local>
2021-05-19 03:47:52 +0800

    initialise

--- practical11/Alignment.py
+++ practical11/Alignment.py
@@ -10,22 +10,57 @@ mouse=c.readlines()
 aminoacid_1=''.join(re.findall(r'[A-Z]',randomseq[1]))
 aminoacid_2=''.join(re.findall(r'[A-Z]',human[1]))
 aminoacid_3=''.join(re.findall(r'[A-Z]',mouse[1]))                 
-edit_distance_1 = 0
-edit_distance_2 = 0
-edit_distance_3 = 0
-for i in range(len(aminoacid_1)):
-        if aminoacid_1[i]!=aminoacid_2[i]:
-                edit_distance_1 += 1
 
-print (edit_distance_1)
+#calculate the percentage
+def percentage(a,b)
+	edit_distance=0
+	for i in range(len(a)):
+		if a[i]!=b[i]:
+                	edit_distance += 1
+	result=(1-edit_distance/len(a))*100
+	return result
 
-for i in range(len(aminoacid_1)):
-        if aminoacid_1[i]!=aminoacid_3[i]:
-                edit_distance_2 += 1
+print('the percentage between human and mouse:'+str(percentage(aminoacid_2,aminoacid_3)))
+print('the percentage between random and mouse:'+str(percentage(aminoacid_1,aminoacid_3)))
+print('the percentage between random and human:'+str(percentage(aminoacid_1,aminoacid_2)))
 
-print (edit_distance_2)
-for i in range(len(aminoacid_2)):
-        if aminoacid_2[i]!=aminoacid_3[i]:
-                edit_distance_3 += 1
+#set a dictionart
+amino_acid = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'X', '*']
+blosum = [
+[ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
+[-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
+[-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
+[-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4],
+[-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1, -4],
+[-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1, -4],
+[-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1, -4],
+[-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1, -4],
+[-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1, -4],
+[-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1, -4],
+[-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1, -4],
+[-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1, -4],
+[-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2, -4],
+[ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0, -4],
+[ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0, -4],
+[-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2, -4],
+[-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1, -4],
+[ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1, -4],
+[-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1, -4],
+[-4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4,  1],
+]
 
-print (edit_distance_3)
+#calculate the goal
+def score(a,b):
+	score=''
+	for i in range (len(a)):
+		score=score+blosum[amino_acid.index(a[i])][amino_acid.index(b[i])]
+	return score
+
+print('the score of human and mouse: '+str(score(aminoacid_2,aminoacid_3)))
+print('the score of human and random seq is '+str(score(aminoacid_2,aminoacid_1)))
+print('the score of mouse and random seq is '+str(score(aminoacid_3,aminoacid_1)))
+
